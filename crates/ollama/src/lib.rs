463f45d454a24c193954cd96d0b0cbbacc2eb9d5
sopaco <dokhell@hotmail.com>
2025-05-12 15:04:34 +0800

    bugfix

--- crates/ollama/src/lib.rs
+++ crates/ollama/src/lib.rs
@@ -23,7 +23,7 @@ struct APIVersionResponse {
 #[cfg(target_os = "windows")]
 static PATH_TO_OLLAMA: &str = "ollama";
 #[cfg(target_os = "macos")]
-static PATH_TO_OLLAMA: &str = "/usr/local/bin/ollama11";
+static PATH_TO_OLLAMA: &str = "/usr/local/bin/ollama";
 #[cfg(target_os = "linux")]
 static PATH_TO_OLLAMA: &str = "/usr/local/bin/ollama";
 
