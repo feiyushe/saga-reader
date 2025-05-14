b6a4c7185ff48d2ecbe9e41a6953b59f4aa19f14
sopaco <dokhell@hotmail.com>
2025-05-14 22:27:30 +0800

    feat(themes): add theme management utilities for light and dark modes
    Introduce functions to get, set, and apply themes, enabling dynamic theme switching and persistence in localStorage. This enhances user experience by allowing theme preferences to be saved and applied consistently.

---
+++ app/src/lib/themes/index.ts
@@ -0,0 +1,21 @@
+import { setTheme as setAppTheme } from '@tauri-apps/api/app';
+
+type ThemePresets = 'light' | 'dark';
+const LITE_STORAGE_KEY_THEME = 'ls_user_theme';
+
+function getTheme(): ThemePresets {
+	return (window.localStorage.getItem(LITE_STORAGE_KEY_THEME) || 'light') as ThemePresets;
+}
+
+function setTheme(theme: ThemePresets) {
+	window.localStorage.setItem(LITE_STORAGE_KEY_THEME, theme);
+	setAppTheme(theme);
+}
+
+function applyTheme() {
+	const theme = getTheme();
+	setAppTheme(theme);
+}
+
+export { applyTheme, getTheme, setTheme };
+export type { ThemePresets };
