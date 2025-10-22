7adb13776c0f05d5c2176302feb42c3e17c1580d
sopaco <dokhell@hotmail.com>
2025-10-22 20:05:33 +0800

    UPDATE WINDOW_EXTERNAL_ENDPOINT_INFORMATION_URL TO webcheck

--- app/src-tauri/src/constrant.rs
+++ app/src-tauri/src/constrant.rs
@@ -3,7 +3,7 @@ pub const WINDOW_MAIN_URL: &str = "/main";
 pub const WINDOW_MAIN_TITLE: &str = "麒睿智库";
 
 pub const WINDOW_EXTERNAL_ENDPOINT_INFORMATION_LABEL: &str = "external_endpoint_information";
-pub const WINDOW_EXTERNAL_ENDPOINT_INFORMATION_URL: &str = "https://ip77.net/";
+pub const WINDOW_EXTERNAL_ENDPOINT_INFORMATION_URL: &str = "https://webcheck.cc/";
 pub const WINDOW_EXTERNAL_ENDPOINT_INFORMATION_TITLE: &str = "网络信息";
 
 pub const WINDOW_ABOUT_LABEL: &str = "about";
